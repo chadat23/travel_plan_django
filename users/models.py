34cf3a04195660d3543139de1e7b6da021f303bf
Chad DeRosier <chad.derosier@gmail.com>
2019-10-16 01:55:09 -0700

    departments added to user profile and migrated

--- users/models.py
+++ users/models.py
@@ -1,6 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+from departments.models import Department
+
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -9,7 +11,7 @@ class Profile(models.Model):
     home_number = models.CharField(max_length=20, null=True, blank=True)
     cell_number = models.CharField(max_length=20, null=True, blank=True)
 
-    # department
+    department: Department = models.ForeignKey(Department, on_delete=models.CASCADE, null=True, blank=True)
 
     active: bool = models.BooleanField(null=True, blank=True)
 
