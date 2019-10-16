bcd1a700cebd34a68614689446050a422e08ccb3
Chad DeRosier <chad.derosier@gmail.com>
2019-10-16 10:26:21 -0700

    a few fields added

--- travel/models.py
+++ travel/models.py
@@ -14,10 +14,11 @@ class Travel(models.Model):
     entry_point: Location = models.ForeignKey(Location, on_delete=models.CASCADE,
                                               related_name='entry_point', null=True, blank=True)
     end_date: datetime = models.DateField(null=True, blank=True)
-    exit_point: Location = models.ForeignKey(Location, on_delete=models.CASCADE,
-                                             related_name='exit_point', null=True, blank=True)
+    exit_point: Location = models.ForeignKey(Location, on_delete=models.CASCADE, null=True, blank=True)
+
+    tracked: bool = models.BooleanField(null=True, blank=True)
+    plb: str = models.CharField(max_length=20, null=True, blank=True)
 
     trip_leader: User = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True)
 
-    plb: str = models.CharField(max_length=20, null=True, blank=True)
     note: str = models.TextField(null=True, blank=True)
--- travel/views.py
+++ travel/views.py
@@ -1,17 +1,52 @@
+from datetime import datetime
+from copy import deepcopy
+
+from django.http import HttpRequest
 from django.shortcuts import render
-from .models import Travel
+from .models import Location, Travel
+
+context = {
+    'title': 'Travel Plan Entry',
+    'start_date': '',
+    'entry_point': '',
+    'end_date': '',
+    'exit_point': '',
+    'tracked': True,
+}
 
 
-def entry(request):
-    context = {
+def entry(request: HttpRequest):
+    if request.method == 'POST':
+        con = _fill_context(request)
 
-    }
-    return render(request, 'travel/entry.html', context)
+        _save_data(con)
+    else:
+        con = context
+    return render(request, 'travel/entry.html', con)
 
 
-def search(request):
-    context = {
-        'title': 'Search',
-        'travels': Travel.objects.all()
-    }
+def search(request: HttpRequest):
     return render(request, 'travel/search.html', context)
+
+
+def _fill_context(request: HttpRequest) -> dict:
+    con = deepcopy(context)
+    con['start_date'] = request.POST['startdate']
+    con['entry_point'] = request.POST['entrypoint']
+    con['end_date'] = request.POST['enddate']
+    con['exit_point'] = request.POST['exitpoint']
+    con['tracked'] = request.POST['tracked'] == 'yes'
+    con['plb'] = request.POST['plb']
+    return con
+
+
+def _save_data(context: dict):
+    travel = Travel()
+    travel.start_date = datetime.strptime(context.get('start_date'), '%Y-%m-%d')
+    travel.entry_point = Location.objects.filter(Location.name == context.get('entry_point'))
+    travel.end_date = context.get('end_date')
+    travel.exit_point = context.get('exit_point')
+    travel.tracked = context.get('tracked')
+    travel.plb = context.get('plb')
+
+    travel.save()
