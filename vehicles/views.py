71f8e9afc2eb8ee5ff0d5642d1bdee288623529b
Chad DeRosier <chad.derosier@gmail.com>
2019-10-20 22:19:28 -0700

    minimized all db retrieved fields

--- vehicles/views.py
+++ vehicles/views.py
@@ -7,17 +7,10 @@ from vehicles.models import Vehicle
 def get_vehicle_by_plate(request: HttpRequest):
     if request.method == 'GET':
         plate = request.GET.get('plate', '').split(' ')[0]
-        vehicle = Vehicle.objects.get(plate=plate)
+        vehicle = Vehicle.objects.filter(plate=plate).values('plate', 'make', 'model', 'color__name').first()
+        vehicle['color'] = vehicle['color__name']
     else:
-        vehicle = Vehicle()
+        vehicle = {}
 
-    color = '' if not vehicle.color else vehicle.color.name
-    response = {'plate': vehicle.plate,
-                'make': vehicle.make,
-                'model': vehicle.model,
-                'color': color,
-                'location': vehicle.location,
-                }
-
-    return JsonResponse(response)
+    return JsonResponse(vehicle)
 
